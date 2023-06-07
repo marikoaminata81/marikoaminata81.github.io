a9c271b7d6aee4c06e24f3f0dd04bad8238474d9
Aminata Mariko <107038393+marikoaminata81@users.noreply.github.com>
2023-06-07 17:59:55 +0000

    Delete js directory

--- js/mail.php
+++
@@ -1,7 +0,0 @@
-
-    <?php
-    $retour = mail('marikoaminata81@gmail.com', 'Envoi depuis la page Contact du Portfolio', $_POST['message'], '');
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
-    
